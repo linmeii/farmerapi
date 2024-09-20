2a2a90b43a171fcefb08ac01fe758f6c93277ef3
Linmeii <lunar1319@gmail.com>
2024-09-20 13:23:51 +0200

    Added console.log for displaying errors on routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,5 +3,8 @@ import { robotStatusController } from "./robotstatus/controller";
 import { storageController } from "./storage/controller";
 
 export const apiRoutes = new Elysia()
+	.onError((error) => {
+		console.log("An error occurred:", error);
+	})
 	.use(robotStatusController)
 	.use(storageController);
